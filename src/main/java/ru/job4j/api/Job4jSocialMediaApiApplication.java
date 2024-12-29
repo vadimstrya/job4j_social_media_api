0d5b6e330a845948d72418c7cf17e440342359c6
vadimstrya <vadimstrya@yandex.ru>
2024-12-29 22:16:36 +0700

    Проект Social Media API [#505090]

---
+++ src/main/java/ru/job4j/api/Job4jSocialMediaApiApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jSocialMediaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+    }
+}
