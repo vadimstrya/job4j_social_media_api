2962deeb764644f421932a1cbd91da2bff7ced53
vadimstrya <vadimstrya@yandex.ru>
2025-01-10 07:29:45 +0700

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/api/repository/UserPostRepository.java
+++ src/main/java/ru/job4j/api/repository/UserPostRepository.java
@@ -1,9 +1,20 @@
 package ru.job4j.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListCrudRepository;
 import ru.job4j.api.entity.UserPost;
+import ru.job4j.api.enums.Statuses;
+
+import java.util.Date;
+import java.util.List;
 
 /** Репозиторий для {@link UserPost} */
 public interface UserPostRepository extends ListCrudRepository<UserPost, Long> {
 
+    List<UserPost> findByUserIdAndStatus(Long userId, Statuses status);
+
+    List<UserPost> findByCreateDateGreaterThanEqualAndCreateDateLessThanEqualAndStatus(Date dateFrom, Date dateTo, Statuses status);
+
+    Page<UserPost> findByStatusOrderByCreateDateAsc(Statuses status, Pageable pageable);
 }
--- src/test/java/ru/job4j/api/repository/UserPostRepositoryTest.java
+++ src/test/java/ru/job4j/api/repository/UserPostRepositoryTest.java
@@ -1,14 +1,18 @@
 package ru.job4j.api.repository;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import ru.job4j.api.entity.User;
 import ru.job4j.api.entity.UserPost;
 import ru.job4j.api.enums.Statuses;
+
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -57,5 +61,91 @@ class UserPostRepositoryTest {
         assertThat(posts).hasSize(1);
         assertThat(posts).extracting(UserPost::getTitle).contains("Заголовок");
         assertThat(posts).extracting(UserPost::getText).contains("Текст");
+    }
+
+    @Test
+    public void whenFindByUserIdAndStatusThenExists() {
+        var user = new User();
+        user.setLogin("John Doe");
+        user.setEmail("john.doe@example.com");
+        user.setPassword("******");
+        user.setStatus(Statuses.A);
+        userRepository.save(user);
+
+        var userPost1 = new UserPost();
+        userPost1.setUserId(user.getId());
+        userPost1.setTitle("Заголовок");
+        userPost1.setText("Текст");
+        userPost1.setStatus(Statuses.A);
+        userPostRepository.save(userPost1);
+
+        var userPost2 = new UserPost();
+        userPost2.setUserId(user.getId());
+        userPost2.setTitle("Заголовок2");
+        userPost2.setText("Текст2");
+        userPost2.setStatus(Statuses.A);
+        userPostRepository.save(userPost2);
+
+        var posts = userPostRepository.findByUserIdAndStatus(user.getId(), Statuses.A);
+        assertThat(posts).hasSize(2);
+    }
+
+    @Test
+    public void whenFindByCreateDateGreaterThanEqualAndCreateDateLessThanEqualAndStatusThenExists() {
+        Date now = new Date();
+        Date yesterday = DateUtils.addDays(now, -1);
+        Date tomorrow = DateUtils.addDays(now, 1);
+
+        var user = new User();
+        user.setLogin("John Doe");
+        user.setEmail("john.doe@example.com");
+        user.setPassword("******");
+        user.setStatus(Statuses.A);
+        userRepository.save(user);
+
+        var userPost1 = new UserPost();
+        userPost1.setUserId(user.getId());
+        userPost1.setTitle("Заголовок");
+        userPost1.setText("Текст");
+        userPost1.setStatus(Statuses.A);
+        userPostRepository.save(userPost1);
+
+        var userPost2 = new UserPost();
+        userPost2.setUserId(user.getId());
+        userPost2.setTitle("Заголовок2");
+        userPost2.setText("Текст2");
+        userPost2.setStatus(Statuses.A);
+        userPostRepository.save(userPost2);
+
+        var posts = userPostRepository.findByCreateDateGreaterThanEqualAndCreateDateLessThanEqualAndStatus(yesterday, tomorrow, Statuses.A);
+        assertThat(posts).hasSize(2);
+    }
+
+    @Test
+    public void whenFindByStatusOrderByCreateDateAscThenExists() {
+        var user = new User();
+        user.setLogin("John Doe");
+        user.setEmail("john.doe@example.com");
+        user.setPassword("******");
+        user.setStatus(Statuses.A);
+        userRepository.save(user);
+
+        var userPost1 = new UserPost();
+        userPost1.setUserId(user.getId());
+        userPost1.setTitle("Заголовок");
+        userPost1.setText("Текст");
+        userPost1.setStatus(Statuses.A);
+        userPostRepository.save(userPost1);
+
+        var userPost2 = new UserPost();
+        userPost2.setUserId(user.getId());
+        userPost2.setTitle("Заголовок2");
+        userPost2.setText("Текст2");
+        userPost2.setStatus(Statuses.A);
+        userPostRepository.save(userPost2);
+
+        var page = userPostRepository.findByStatusOrderByCreateDateAsc(Statuses.A, Pageable.ofSize(1));
+        assertThat(page.getTotalElements()).isEqualTo(2);
+        assertThat(page.getTotalPages()).isEqualTo(2);
     }
 }
